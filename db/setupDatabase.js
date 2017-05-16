9e59d5d37f0ff898d1588bc70d80d2236697176a
joshuastagner <joshuastagner@gmail.com>
2017-05-16 14:27:24 -0700

    Add config env variables to setupDatabase.js

--- db/setupDatabase.js
+++ db/setupDatabase.js
@@ -6,7 +6,12 @@ const url = require('./dbLocalConnection.js');
 
 var db = require('knex')({
   client: 'pg',
-  connection: url
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
 });
 
 db.schema.hasTable('trends').then(function (exists) {
