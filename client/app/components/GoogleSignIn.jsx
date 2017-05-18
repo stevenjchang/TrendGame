1c74733df158276963d8bcc2d8c72bfe59b18348
Yujin Chung <yujin0chung@gmail.com>
2017-05-17 19:35:04 -0700

    Creates GoogleSignIn component

---
+++ client/app/components/GoogleSignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoogleSignIn = (props) => {
+  return (
+    <a href="/auth/google">
+      <span class="fa fa-google-plus"></span> Google Sign-In
+    </a>
+    );
+}
+
+export default GoogleSignIn;
