1b08bc10114ee0bdd7562dfc8b51a087b324cb98
Steve <stevenjchang@gmail.com>
2017-05-18 14:48:53 -0700

    import App.jsx

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -4,10 +4,12 @@ import axios from 'axios';
 import Layout from './components/Layout';
 import formatQuery from './../../utilities/formatQuery.js';
 import { HashHistory as Router, Route } from 'react-router-dom';
-
+import App from './components/App';
 
 render(
+  <Router>
   <App/>
+  </Router>
   
   , document.getElementById('app')
 );
