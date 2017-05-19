59f82633388070907dbfef22524dbd96414804d0
Yujin Chung <yujin0chung@gmail.com>
2017-05-18 19:25:37 -0700

    Sends userId as paramter to retrieve trends handler

--- server.js
+++ server.js
@@ -146,7 +146,13 @@ app.post('/api/history', (req, res) => {
 });
 
 app.get('/api/history', (req, res) => {
-  queries.getSearches(10, (err, data) => {
+  let userId;
+  if (req.session.user === undefined) {
+    userId = null;
+  } else {
+    userId = req.session.user[0].id
+  }
+  queries.getSearches(10, userId, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -157,15 +163,15 @@ app.get('/api/history', (req, res) => {
 
 app.get('/api/history/user', (req, res) => {
   console.log('history/user endpoint has been hit!');
-  res.send(['query1', 'query2', 'query3'])
-  // let userId = req.session.user[0].id;
-  // queries.getUserSearches(10, userId, (err, data) => {
-  //   if (err) {
-  //     res.status(500).send(err);
-  //   } else {
-  //     res.status(200).send(data);
-  //   }
-  // });
+  // res.send(['query1', 'query2', 'query3'])
+  let userId = req.session.user[0].id;
+  queries.getUserSearches(10, userId, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 app.get('/api/worker', (req, res) => {
