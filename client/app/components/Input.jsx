2d2afabc9a0c5d14cec7aae9e2272fc7c1a00d92
Scott Moschella <scottmosch@gmail.com>
2017-05-19 14:02:11 -0700

    Add speech recognition functionality to the input

--- client/app/components/Input.jsx
+++ client/app/components/Input.jsx
@@ -13,13 +13,16 @@ export default class Input extends React.Component {
     this.state = {
       displayTrend: '',
       startTime: '',
-      endTime: ''
+      endTime: '',
+      listening: false
       // theStream: null
     };
     this.handeInput = this.handeInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    this.startDictation = this.startDictation.bind(this);
+    this.eatClick = this.eatClick.bind(this);
   }
 
   componentWillReceiveProps() {
@@ -39,7 +42,15 @@ export default class Input extends React.Component {
   }
 
   handleSubmit(e) {
+<<<<<<< HEAD
     e.preventDefault();
+=======
+    if(e){
+      e.preventDefault();
+    }
+
+    document.querySelector('.search-input').blur();
+>>>>>>> Add speech recognition functionality to the input
     this.props.collectData(this.state.trend, this.state.startTime, this.state.endTime)
     window.location.href = 'http://127.0.0.1:8080/#/' + this.state.trend.split(' ').join('+');
   }
@@ -60,7 +71,39 @@ export default class Input extends React.Component {
     });
   }
 
+  startDictation() {
+    console.log("started");
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+      this.setState({listening: true});
 
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = (e) => {
+        document.getElementById('transcript').value = e.results[0][0].transcript;
+        this.setState({trend: document.getElementById('transcript').value});
+        this.handleSubmit();
+        recognition.stop();
+        this.setState({listening: false});
+        // document.getElementById('the-form').submit();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+        this.setState({listening: false});
+      }
+
+    }
+  }
+
+  eatClick(e) {
+    e.preventDefault();
+  }
 
   // handleStartRecord(e) {
   //   this.state.theStream
@@ -71,24 +114,33 @@ export default class Input extends React.Component {
   // }
 
   render () {
+    let micIcon;
+    if (this.state.listening) {
+      micIcon = <i className="fa fa-circle-o-notch fa-fw fa-spin mic-icon"></i>
+    } else {
+      micIcon = <i className="fa fa-microphone fa-fw mic-icon" aria-hidden="true"></i>
+    }
+
     return (
       <div className="row mb-4">
         <div className="col">
           {/*<button className="get-audio" onMouseDown={this.handleStartRecord} onMouseUp={this.handleEndRecord}>Hello</button>*/}
           <form 
+            id="the-form"
             action="submit"
             onSubmit={this.handleSubmit}
           >
             <div className="input-group speech">
               <input
                 value={this.state.trend}
+                id="transcript"
                 className="form-control search-input"
                 type="text"
                 placeholder="Enter a topic"
                 onChange={this.handeInput}
                 autoFocus
               >
-              </input>
+              </input><a href="" onClick={this.eatClick}><div className="mic-button" onClick={this.startDictation}>{micIcon}</div></a>
               <span className="input-group-btn">
                 <button className="btn btn-primary" type="submit">
                     Search
