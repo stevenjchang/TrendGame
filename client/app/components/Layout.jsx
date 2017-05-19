110842b0fd802958a9982fada4bb4c4d9286013e
joshuastagner <joshuastagner@gmail.com>
2017-05-18 18:37:20 -0700

    Modify flow of props through app

--- client/app/components/Layout.jsx
+++ client/app/components/Layout.jsx
@@ -3,7 +3,7 @@ import Body from './Body';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout = ({ selectedDate, chartData, collectData, storyPoint, history, addStart, addEnd, getChartClick}) => {
+const Layout = ({ selectedDate, chartData, collectData, storyPoint, history, addStart, addEnd, getChartClick, setTrend, trend}) => {
   return (
     <div>
       <div className="container">
@@ -17,6 +17,8 @@ const Layout = ({ selectedDate, chartData, collectData, storyPoint, history, add
           history={history}
           getChartClick={getChartClick}
           selectedDate={selectedDate}
+          setTrend={setTrend}
+          trend={trend}
         />
       </div>
       <Footer/>
