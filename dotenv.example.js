2012e0ee21092d32289175201f2cefb2d9577806
Yujin Chung <yujin0chung@gmail.com>
2017-05-19 10:54:02 -0700

    Adds variables to env dummy file

--- dotenv.example.js
+++ dotenv.example.js
@@ -8,3 +8,10 @@ DB_NAME=trendGame
 PORT=8080
 HOST=localhost
 
+ROOT_URL=http://127.0.0.1:8080
+
+GOOGLE_CLIENT_ID=clientId
+GOOGLE_CLIENT_SECRET=clientsecret
+GOOGLE_CALLBACKURL=http://localhost:8080/auth/google/callback
+
+SESSION_SECRET=garbage
