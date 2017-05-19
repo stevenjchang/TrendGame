c3359773d69019d9c5bf3432466deabc6901c2ac
Scott Moschella <scottmosch@gmail.com>
2017-05-19 14:01:35 -0700

    Fix 'className' on Google plus icon

--- client/app/components/GoogleSignIn.jsx
+++ client/app/components/GoogleSignIn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GoogleSignIn = (props) => {
   return (
     <a href="/auth/google">
-      <span class="fa fa-google-plus"></span> Google Sign-In
+      <span className="fa fa-google-plus"></span> Google Sign-In
     </a>
     );
 }
