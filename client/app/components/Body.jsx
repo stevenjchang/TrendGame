43c3bdb421b90965137141b6f4e99bf04561e6c2
joshuastagner <joshuastagner@gmail.com>
2017-05-18 18:34:01 -0700

    Modify props moving through app

--- client/app/components/Body.jsx
+++ client/app/components/Body.jsx
@@ -4,7 +4,8 @@ import Input from './Input';
 import History from './History';
 import ArticleList from './ArticleList';
 
-const Body = ({ selectedDate, collectData, history, chartData, storyPoint, addStart, addEnd, getChartClick }) => {
+const Body = ({ selectedDate, collectData, history, chartData, storyPoint, addStart, addEnd, setTrend, trend, getChartClick }) => {
+
   return (
     <div className="row">
       <div className="col col-m-10 offset-m-1 col-lg-8 offset-lg-2">
@@ -12,6 +13,8 @@ const Body = ({ selectedDate, collectData, history, chartData, storyPoint, addSt
           collectData={collectData}
           addStart={addStart}
           addEnd={addEnd}
+          setTrend={setTrend}
+          trend={trend}
         />
         <History history={history} collectData={collectData}/>
         <TrendChart getChartClick={getChartClick} chartData={chartData} storyPoint={storyPoint}/>
