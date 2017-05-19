82195537956654d8f94aaad1a09899cd2cda21db
joshuastagner <joshuastagner@gmail.com>
2017-05-18 18:34:38 -0700

    Make links clickable with react router

--- client/app/components/HistoryItem.jsx
+++ client/app/components/HistoryItem.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const HistoryItem = ({ term, index, collectData }) => {
   let element = index === 0
     ? (
         <li className="list-inline-item">
+<<<<<<< HEAD
           <a href={'/#/'+ term} className="text-muted" onClick={() => collectData(term)}>{term}</a>
+=======
+          <a href={'/#/' + term} className="text-muted" onClick={() => collectData(term)}>{term}</a>
+>>>>>>> Make links clickable with react router
         </li>
       )
     : (
         <li className="list-inline-item">
+<<<<<<< HEAD
           &middot; <a href={'/#/' + term} className="text-muted" onClick={() => collectData(term)}>{term}</a>
+=======
+          &middot; <a href={'/#/' + term.split(' ').join('+')} className="text-muted" onClick={() => collectData(term)}>{term}</a>
+>>>>>>> Make links clickable with react router
         </li>
       );
 
