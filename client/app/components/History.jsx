76b99737d0820f0e8d0f3924730c7ad130fc105d
Yujin Chung <yujin0chung@gmail.com>
2017-05-18 19:26:36 -0700

    Adds userHistory so that user specific searches are rendered

--- client/app/components/History.jsx
+++ client/app/components/History.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import HistoryItem from './HistoryItem';
 
-const History = ({ history, collectData }) => {
+const History = ({ history, userHistory, collectData }) => {
+  {console.log('userHistory&&&&&&&', userHistory)}
   return (
     <div className="row mb-5">
+
       <div className="col text-center">
         <small>Recent searches</small>
         <ul className="list-inline text-center text-muted">
           {history.map((term, index) => {
             return <HistoryItem key={term} term={term} index={index} collectData={collectData}/>;
+          })}
+        </ul>
+      </div>
+
+      <div className="col text-center">
+        <small>My searches</small>
+        <ul className="list-inline text-center text-muted">
+          {userHistory === undefined ? 
+            <div></div> : 
+            userHistory.map((term, index) => {
+            return <HistoryItem key={term} term={term.name} index={index} collectData={collectData}/>;
           })}
         </ul>
       </div>
