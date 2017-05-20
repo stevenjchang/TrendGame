89824ceb160a2b0f911fba749611a816a8713635
Yujin Chung <yujin0chung@gmail.com>
2017-05-20 14:19:30 -0700

    Moves log in to Header component

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import GoogleSignIn from './GoogleSignIn';
+import ArticleList from './ArticleList';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 export default class Header extends React.Component {
   render () {
+    let loggedIn;
+    if (cookies.get('loggedIn') === 'true') {
+      loggedIn = <div>Welcome Back! <a href="/logout">Logout</a></div>
+    } else {
+      loggedIn = <GoogleSignIn />
+    }
     return (
       <div className="row">
         <div className="col mt-4 mb-4 text-center">
+          {loggedIn}
           <h1>TrendGame</h1>
           <p className="text-muted">
             Find out <strong>when</strong> interest in a topic peaked and <strong>why.</strong>
