d15191fb7b3bf2311e12a6233048afbf5187ff41
Steve <stevenjchang@gmail.com>
2017-05-19 22:45:27 -0700

    resolved conflict

--- client/app/components/Chart.jsx
+++ client/app/components/Chart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Chart } from 'react-google-charts';
 
-<<<<<<< HEAD
+
 class TrendChart extends React.Component {
   constructor(props) {
     super(props)
@@ -9,53 +9,13 @@ class TrendChart extends React.Component {
       {
         eventName: 'select',
         callback: (Chart) => {
-          const selectedPoint = Chart.chart.getSelection()[0]
-            // Returns Chart so you can access props
-          if(selectedPoint) {
-            this.handleChartClick(this.props.chartData.data[selectedPoint.row + 1][0]);
-          }
+          
+            // Returns Chart so you can access props and  the ChartWrapper object from chart.wrapper
+          console.log('Selected ', this.props.chartData.data[Chart.chart.getSelection()[0].row + 1][0]);
         },
       }
     ];
     this.chartEvents[0].callback = this.chartEvents[0].callback.bind(this);
-=======
-const TrendChart = ({ chartData, storyPoint }) => {
-  let displayChart;
-  let { data, trend, start, end, loader } = chartData;
-  console.log('This is the end: ', end);
-  if (data.length === 0 || loader !== false) {
-    displayChart = loader;
-  } else {
-    displayChart = (
-      <span>
-        <h2 className="h4 mb-4">
-          <strong>When</strong> did interest in <strong className="text-lowercase">{trend}</strong> peak? <strong>{storyPoint.formattedAxisTime}</strong>
-        </h2>
-        <Chart
-          chartType="LineChart"
-          data={data}
-          options={{
-            hAxis: { title: null, minValue: new Date(start * 1000), maxValue: new Date(end * 1000), gridlines: { color: 'none' } },
-            vAxis: { title: null, minValue: 0, maxValue: 100 },
-            chartArea: { width: '90%', height: '80%' },
-            legend: 'none',
-            series: {
-              0: { color: '#dc3c3c' }
-            },
-            lineWidth: 3
-          }}
-          graph_id="LineChart"
-          width="100%"
-          height="400px"
-          legend_toggle
-        />
-      </span>
-    );
->>>>>>> Add date picking / trend zoom
-  }
-
-  handleChartClick(dateClicked){
-    this.props.getChartClick(dateClicked);
   }
 
   render() {
