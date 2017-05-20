c8f8e03087ff4b9fa64492117817d5d865d79902
Yujin Chung <yujin0chung@gmail.com>
2017-05-20 16:41:13 -0700

    Renders user's name and image

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -9,15 +9,21 @@ export default class Header extends React.Component {
   render () {
     let loggedIn;
     if (cookies.get('loggedIn') === 'true') {
-      loggedIn = <div>Welcome Back! <a href="/logout">Logout</a></div>
+      loggedIn = 
+      <div>
+        <div className="logout"><a href="/logout">Logout</a></div>
+        <div className="welcome">
+          <div>Welcome back, {this.props.userInfo[0]}! <img className="user-image" src={this.props.userInfo[1]} /></div>
+        </div>
+      </div>
     } else {
-      loggedIn = <GoogleSignIn />
+      loggedIn = <div className="login"><GoogleSignIn /></div>
     }
     return (
       <div className="row">
         <div className="col mt-4 mb-4 text-center">
           {loggedIn}
-          <h1>TrendGame</h1>
+          <h1 style={{clear: 'both'}}>TrendGame</h1>
           <p className="text-muted">
             Find out <strong>when</strong> interest in a topic peaked and <strong>why.</strong>
           </p>
